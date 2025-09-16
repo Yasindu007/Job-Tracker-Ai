0e73dea2a3495c28198ffd4eff533f5e0bdf862f
Yasindu007 <malithdesilva64@gmail.com>
2025-09-16 14:35:33 +0530

    ccdc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { StackProvider, StackTheme } from "@stackframe/stack";
 import { client } from "@/stack"; // Import client from the client-side stack.ts
-import { stackServerApp } from "../stack/server"; // Corrected import path
+
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/components/providers'
